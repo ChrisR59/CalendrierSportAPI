c30581bd24ab3b741ba7b1f5e115e8c614d3455a
Zalendil <noctis_59@yahoo.fr>
2020-12-10 15:24:21 +0100

    ajout members controller

---
+++ AgendaSportif/Controllers/MembersController.cs
@@ -0,0 +1,32 @@
+﻿using AgendaSportif.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaSportif.Controllers
+{
+    public class MembersController : Controller
+    {
+        private DataDbContext data;
+
+        public EventController(DataDbContext _data)
+        {
+            data = _data;
+        }
+
+        // POST: api/Event
+        [Route("/AddMembers")]
+        [HttpPost]
+        public ActionResult Post([FromBody] Members members)
+        {
+            data.Members.Add(members);
+
+            if (data.SaveChanges() > 0)
+                return Ok(new { error = false });
+            else
+                return StatusCode(500);
+        }
+    }
+}
